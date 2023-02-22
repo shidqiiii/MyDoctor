7e5beb566795a0b96582a0c9087d517f7bad883b
Daffa SH <shidqidaffa@gmail.com>
2023-02-22 13:12:41 +0700

    create splash screen page

---
+++ src/assets/illustration/index.js
@@ -0,0 +1,4 @@
+import ILLogo from './ILLogo.svg';
+import ILGetStarted from './get-started.png';
+
+export {ILLogo, ILGetStarted};
---
+++ src/pages/Splash/index.js
@@ -0,0 +1,29 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {ILLogo} from '../../assets';
+
+const Splash = () => {
+  return (
+    <View style={styles.container}>
+      <ILLogo />
+      <Text style={styles.title}>My Doctor</Text>
+    </View>
+  );
+};
+
+export default Splash;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#112340',
+    marginTop: 20,
+  },
+});
