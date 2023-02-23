488389934277b4376836f49ad620152473365c30
Daffa SH <shidqidaffa@gmail.com>
2023-02-23 10:22:53 +0700

    update colors

---
+++ src/utils/colors/index.js
@@ -0,0 +1,30 @@
+const mainColors = {
+  green1: '#0bcad4',
+  dark1: '#112340',
+  grey1: '#7d8797',
+  grey2: '#e9e9e9',
+  white1: '#ffffff',
+  black1: '#000000',
+};
+
+export const colors = {
+  primary: mainColors.green1,
+  secondary: mainColors.dark1,
+  white: mainColors.white1,
+  black: mainColors.black1,
+  text: {
+    primary: mainColors.dark1,
+    secondary: mainColors.grey1,
+  },
+  button: {
+    primary: {
+      background: mainColors.green1,
+      text: mainColors.white1,
+    },
+    secondary: {
+      background: mainColors.white1,
+      text: mainColors.dark1,
+    },
+  },
+  border: mainColors.grey2,
+};
