a205c39973d06d4e4acc8f306b22490683e72ad7
Daffa SH <shidqidaffa@gmail.com>
2023-02-24 14:04:15 +0700

    import asset dummy & illustration

---
+++ src/assets/dummy/index.js
@@ -0,0 +1,17 @@
+import DummyDoctor1 from './dok1.png';
+import DummyDoctor2 from './dok2.png';
+import DummyDoctor3 from './dok3.png';
+import DummyNews1 from './news1.png';
+import DummyNews2 from './news2.png';
+import DummyNews3 from './news3.png';
+import DummyUser from './user.png';
+
+export {
+  DummyUser,
+  DummyDoctor1,
+  DummyDoctor2,
+  DummyDoctor3,
+  DummyNews1,
+  DummyNews2,
+  DummyNews3,
+};
--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,5 +1,17 @@
+import ILCatAnak from './cat-dok-anak.svg';
+import ILCatObat from './cat-dok-obat.svg';
+import ILCatPsikiater from './cat-dok-psikiater.svg';
+import ILCatUmum from './cat-dok-umum.svg';
 import ILGetStarted from './get-started.png';
 import ILLogo from './ILLogo.svg';
 import ILNullPhoto from './null-photo.png';
 
-export {ILLogo, ILGetStarted, ILNullPhoto};
+export {
+  ILLogo,
+  ILGetStarted,
+  ILNullPhoto,
+  ILCatAnak,
+  ILCatObat,
+  ILCatPsikiater,
+  ILCatUmum,
+};
