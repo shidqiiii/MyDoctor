7ecca99c4958ca42988877760f3dd7305b95dd6b
Daffa SH <shidqidaffa@gmail.com>
2023-03-01 11:19:06 +0700

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import firebase from '@react-native-firebase/app';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 
 import Router from './routes';
 
 const App = () => {
+  const apps = firebase.apps;
+
+  apps.forEach(app => {
+    console.log('App name: ', app.options);
+  });
+
   return (
     <NavigationContainer>
       <Router />
