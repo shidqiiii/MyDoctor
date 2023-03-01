d3ebe480bb18e8b07d1db08ae1a19003ef2705ed
Daffa SH <shidqidaffa@gmail.com>
2023-03-01 08:42:14 +0700

    set onFocus onBlur

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {Button, Gap, Header, Input} from '../../components';
 import {colors} from '../../utils';
@@ -8,18 +8,20 @@ const Register = ({navigation}) => {
     <View style={styles.container}>
       <Header title="Daftar Akun" onPress={() => navigation.goBack()} />
       <View style={styles.content}>
-        <Input label="Full Name" />
-        <Gap height={24} />
-        <Input label="Pekerjaan" />
-        <Gap height={24} />
-        <Input label="Email" />
-        <Gap height={24} />
-        <Input label="Password" />
-        <Gap height={40} />
-        <Button
-          title="Continue"
-          onPress={() => navigation.navigate('UploadPhoto')}
-        />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Input label="Full Name" />
+          <Gap height={24} />
+          <Input label="Pekerjaan" />
+          <Gap height={24} />
+          <Input label="Email" />
+          <Gap height={24} />
+          <Input label="Password" />
+          <Gap height={40} />
+          <Button
+            title="Continue"
+            onPress={() => navigation.navigate('UploadPhoto')}
+          />
+        </ScrollView>
       </View>
     </View>
   );
