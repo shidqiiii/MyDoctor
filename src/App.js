60e47b2ee0e1591c47c8543626970de8414d97bf
Daffa SH <shidqidaffa@gmail.com>
2023-03-01 09:37:22 +0700

    new app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import firebase from '@react-native-firebase/app';
 
 import Router from './routes';
-
 const App = () => {
+  const apps = firebase.apps;
+
+  apps.forEach(app => {
+    console.log('App name: ', app.options);
+  });
+
   return (
     <NavigationContainer>
       <Router />
